218d2a45640ceec36603c8b4c11509817ba40694
NumesSanguis <stef_struijk@hotmail.com>
2016-03-06 22:27:53 +0100

    Making use of StringProperty()

--- test/kv_reference/magic.py
+++ test/kv_reference/magic.py
@@ -3,9 +3,11 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 
 
 class MagicLayout(BoxLayout):
+    m_text = StringProperty("Should change to text from id: important_text")
 
     def change_text(self):
         print("Chaning text")
